912d713c2e8c89c4f251d8eda0d0dde0d25a618e
jmayta1984 <jmayta@pucp.edu.pe>
2025-09-15 15:53:36 -0500

    feat: update product detail screen

--- EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/ProductDetail.kt
+++ EasyShop/app/src/main/java/pe/edu/upc/easyshop/features/home/presentation/ProductDetail.kt
@@ -1,16 +1,22 @@
 package pe.edu.upc.easyshop.features.home.presentation
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,10 +26,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import pe.edu.upc.easyshop.core.ui.components.RoundedIcon
 import pe.edu.upc.easyshop.core.ui.theme.AppTheme
 import pe.edu.upc.easyshop.shared.models.Product
 import pe.edu.upc.easyshop.shared.models.products
@@ -33,13 +39,26 @@ fun ProductDetail(product: Product) {
     Scaffold(
         floatingActionButton = {
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Button(onClick = {}, modifier = Modifier.weight(1f)) {
+                Button(
+                    onClick = {}, modifier = Modifier.weight(1f),
+                    colors = ButtonDefaults.buttonColors(
+                        contentColor = MaterialTheme.colorScheme.primary,
+                        containerColor = MaterialTheme.colorScheme.onPrimary
+                    ),
+                    border = BorderStroke(
+                        1.dp,
+                        MaterialTheme.colorScheme.primary
+                    )
+                ) {
                     Text("Add to cart")
                 }
 
+                Spacer(modifier = Modifier.width(8.dp))
                 Button(onClick = {}, modifier = Modifier.weight(1f)) {
                     Text("Buy now")
                 }
@@ -72,7 +91,26 @@ fun ProductDetail(product: Product) {
                     )
                 }
             }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth().padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(product.name, style = MaterialTheme.typography.titleMedium)
+                Text("$ ${product.price}", style = MaterialTheme.typography.titleMedium)
+            }
 
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding( 8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                RoundedIcon(Icons.Default.Remove)
+                Text("1", modifier = Modifier.padding(horizontal = 8.dp))
+                RoundedIcon(Icons.Default.Add)
+            }
         }
     }
 
