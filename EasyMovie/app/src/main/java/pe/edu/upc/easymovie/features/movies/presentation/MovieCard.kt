3bc1cbbaee3f0702b48169fad44b26c4721ae44b
jmayta1984 <jmayta@pucp.edu.pe>
2025-10-06 13:37:53 -0500

    feat (movies): add padding to MovieCard

--- EasyMovie/app/src/main/java/pe/edu/upc/easymovie/features/movies/presentation/MovieCard.kt
+++ EasyMovie/app/src/main/java/pe/edu/upc/easymovie/features/movies/presentation/MovieCard.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Card
@@ -13,6 +15,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -27,7 +30,9 @@ fun MovieCard(
     onToggleFavorite: () -> Unit
 ) {
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
         onClick = onClick
     ) {
         Row {
@@ -36,10 +41,13 @@ fun MovieCard(
                 contentDescription = null,
                 modifier = Modifier
                     .height(96.dp)
-                    .width(56.dp),
-                contentScale = ContentScale.Fit
+                    .width(56.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.FillHeight
             )
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 8.dp)) {
                 Text(
                     movie.title, maxLines = 1,
                     fontWeight = FontWeight.SemiBold
